d83af51e4b33efa2fec4eefe1a3673e4ea91c8f9
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-22 08:53:21 -0700

    reafactored sort functions to merge def

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,42 +1,57 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+def merge(left, right):
+    sortedArray = [0] * (len(left) + len(right))
+		
+    # k is a pointer for the sortedArray current index position
+    # i is a position pointer for the left half Array
+    # j is a position pointer for the right half Array
+    i = j = k = 0
     
-    return merged_arr
+    # while there still is numbers in the left and right array
+    # i and j still point to a index position
+    while i < len(left) and j < len(right):
+        # if left[i] position is less then right[j]
+        # add it to the sortedArray[k] position
+        if left[i] <= right[j]:
+            sortedArray[k] = left[i]
+            i += 1 # then increment left[i] posiiton
+        else:
+            sortedArray[k] = right[j]
+            j += 1
+        # incrememnt k position since were 
+        # done with that index position
+        k += 1
+    
+    # if theres number remaining in left array
+    # add them to the sortedArray
+    while i < len(left):
+        sortedArray[k] = left[i]
+        # incrememnt after done adding numbers
+        i += 1
+        k += 1
+        
+    # if theres number remaining in right array
+    # add them to the sortedArray
+    while j < len(right):
+        sortedArray[k] = right[j]
+        # incrememnt after done adding numbers
+        j += 1
+        k += 1
+        
+    return sortedArray
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     if len(arr) > 1: 
         mid = len(arr) // 2 
-        Left = arr[:mid] 
-        Right = arr[mid:]
+        left = arr[:mid] 
+        right = arr[mid:]
 
         # Recursion
-        merge_sort(Left)
-        merge_sort(Right)
+        merge_sort(left)
+        merge_sort(right)
 
-        i = j = k = 0
-
-        while i < len(Left) and j < len(Right): 
-            if Left[i] < Right[j]: 
-                arr[k] = Left[i] 
-                i += 1
-            else: 
-                arr[k] = Right[j] 
-                j += 1
-            k += 1
-
-        # Check if any element was left.
-        while i < len(Left): 
-            arr[k] = Left[i] 
-            i += 1
-            k += 1
-
-        while j < len(Right): 
-            arr[k] = Right[j] 
-            j += 1
-            k += 1
+        return merge(merge_sort(left), merge_sort(right))
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
@@ -52,7 +67,7 @@ def merge_in_place(arr, start, mid, end):
       
     # Two pointers to maintain start 
     # of both arrays to merge 
-    while (start <= mid and start2 <= end): 
+    while start <= mid and start2 <= end: 
   
         # If element 1 is in right place 
         if (arr[start] <= arr[start2]): 
@@ -75,7 +90,7 @@ def merge_in_place(arr, start, mid, end):
             start2 += 1; 
 
 
-def merge_sort_in_place(arr, l, r):
+def merge_sort_in_place(arr, left, right):
     if (left < right): 
   
         # Same as (left + right) / 2, but avoids overflow 
